49846d1c958198ad2c80536daca483ef32422348
Coty Condry <ccondry@cisco.com>
2020-12-08 11:04:14 -0600

    update fetch action, remove extra getUsers action

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -65,46 +65,83 @@ const mutations = {
 }
 
 const actions = {
-  async fetch ({getters}, {url, options = {}}) {
+  async fetch ({commit, getters, dispatch}, {
+    group,
+    type,
+    url,
+    options = {},
+    mutation,
+    message,
+    showNotification = true,
+    onError,
+    transform
+  }) {
+    message = message || `${options.method === 'POST' ? 'save' : 'get'} ${group} ${type}`
     if (!url) {
-      throw Error('url is a required parameter for fetch')
+      throw Error('url is a required parameter for fetch ' + message)
     }
-    // set content type to JSON by default
-    options.headers = options.headers || {}
-    options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json'
-    
-    // stringify body if object
-    if (typeof options.body === 'object') {
-      options.body = JSON.stringify(options.body)
-    }
-    // console.log('fetch', url, options)
-    // add query parameters to URL
+    console.log(`${message}...`)
+    const loadingOrWorking = !options.method || options.method === 'GET' ? 'setLoading' : 'setWorking'
+    dispatch(loadingOrWorking, {group, type, value: true})
     try {
-      // console.log('url', url)
+      // const data = await dispatch('fetch', {url, options})
+      // set default headers
+      options.headers = options.headers || {}
+      // set content type to JSON by default
+      options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json'
+      // set accept to JSON by default
+      options.headers['Accept'] = options.headers['Accept'] || 'application/json'
+      // set JWT auth header by default
+      options.headers['Authorization'] = options.headers['Authorization'] || 'Bearer ' + getters.jwt
+      // set instant demo instance name
+      // options.headers['Instance'] = getters.instanceName
+      // stringify body if it is an object
+      if (typeof options.body === 'object') {
+        options.body = JSON.stringify(options.body)
+      }
+      // add query parameters to URL
       const endpoint = addUrlQueryParams(url, options.query)
       // console.log('endpoint', endpoint)
       const response = await window.fetch(endpoint, options)
+      // get the response body as text
       const text = await response.text()
+      // response code 200 - 299?
       if (response.ok) {
         try {
-          return JSON.parse(text)
+          // parse response text into JSON
+          const json = JSON.parse(text)
+          console.log(`${message} success:`, json)
+          if (typeof mutation === 'string') {
+            if (typeof transform === 'function') {
+              // put transformed JSON data into state
+              commit(mutation, transform(json))
+            } else {
+              // put JSON data into state
+              commit(mutation, json)
+            }
+          }
+          return json
         } catch (e) {
+          // body is not json data. return the raw text, and don't attempt
+          // to put it into state
+          console.log(`${message} success:`, text)
           return text
         }
       } else if (response.status === 401) {
         // expired JWT. forget jwt and forward to SSO login
         window.localStorage.removeItem('jwt')
-        window.location = getters.ssoUrl
+        return dispatch('login')
       } else {
         // not OK and not 401
         let m = text
         try {
           const json = JSON.parse(text)
-          m = json.message || json.apiError || json[Object.keys(json)[0]]
+          m = json.message || json.apiError || json.error_description || json[Object.keys(json)[0]]
         } catch (e) {
+          // use empty string instead of text/html content
           const regex = /text\/html/i
           if (response.headers.get('content-type').match(regex)) {
-            // text/html - don't return that whole thing
+            console.log('removing html response from message')
             m = ''
           }
         }
@@ -117,11 +154,23 @@ const actions = {
         error.status = response.status
         error.statusText = response.statusText
         error.text = m
-        throw error
+        // throw error
+        if (typeof onError === 'function') {
+          onError(error)
+        }
       }
     } catch (e) {
-      // just rethrow any other errors, like connection timeouts
-      throw e
+      console.error(`${message} failed: ${e.message}`)
+      if (showNotification) {
+        Toast.open({
+          message: `Failed to ${message}: ${e.message}`,
+          type: 'is-danger',
+          duration: 6 * 1000,
+          queue: false
+        })
+      }
+    } finally {
+      dispatch(loadingOrWorking, {group, type, value: false})
     }
   },
   setWorking ({commit}, {group, type, value = true}) {
@@ -129,28 +178,6 @@ const actions = {
   },
   setLoading ({commit}, {group, type, value = true}) {
     commit(types.SET_LOADING, {group, type, value})
-  },
-  async getUsers ({commit, dispatch, getters}) {
-    // get system environment info
-    dispatch('setLoading', {group: 'app', type: 'demo', value: true})
-    try {
-      const url = getters.endpoints.demo
-      const options = {
-        headers: {
-          Authorization: 'Bearer ' + getters.jwt
-        }
-      }
-      const environment = await dispatch('fetch', {url, options})
-      commit(types.SET_ENVIRONMENT, environment)
-    } catch (e) {
-      Toast.open({
-        message: 'Failed to load demo environment information: ' + e.message,
-        duration: 10 * 1000,
-        type: 'is-danger'
-      })
-    } finally {
-      dispatch('setWorking', {group: 'app', type: 'demo', value: false})
-    }
   }
 }
 
